922d66a3f95528f5b65ea3aa8a0cf50843b466d3
mattchley <mattchley@gmail.com>
2019-10-22 12:40:04 -0700

    setCityJumbotromFunctionality

--- weather.js
+++ weather.js
@@ -1,57 +1,27 @@
+// Variables
+var cityInput = $('#city-search')
+var mainWeatherEL = $('.main')
+var fiveDayEL = $('.5-day')
 
-unction buildQueryURL() {
-    // queryURL is the url we'll use to query the API
-    var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?";
-  
-    // Begin building an object to contain our API call's query parameters
-    // Set the API key
-    var queryParams = { "api-key": "R1a31F4tBjCUaM2ho8GtIFsrSdtXt30M" };
-  
-    // Grab text the user typed into the search input, add to the queryParams object
-    queryParams.q = $("#search-term")
-      .val()
-      .trim();
-  
-    // If the user provides a startYear, include it in the queryParams object
-    var startYear = $("#start-year")
-      .val()
-      .trim();
-  
-    if (parseInt(startYear)) {
-      queryParams.begin_date = startYear + "0101";
-    }
-  
-    // If the user provides an endYear, include it in the queryParams object
-    var endYear = $("#end-year")
-      .val()
-      .trim();
-  
-    if (parseInt(endYear)) {
-      queryParams.end_date = endYear + "0101";
-    }
-  
-    // Logging the URL so we have access to it for troubleshooting
-    console.log("---------------\nURL: " + queryURL + "\n---------------");
-    console.log(queryURL + $.param(queryParams));
-    return queryURL + $.param(queryParams);
-  }
+function buildQueryURL() {
+    var APIKey = "3a92bc0a0ca29575a3569a00c0268022";
+    var queryURL = "https://api.openweathermap.org/data/2.5/weather?" +
+        "q=" + cityInput + "&units=imperial&appid=" + APIKey;
 
-$("#city-search").on("click", function(event) {
-    // This line allows us to take advantage of the HTML "submit" property
-    // This way we can hit enter on the keyboard and it registers the search
-    // (in addition to clicks). Prevents the page from reloading on form submit.
-    event.preventDefault();
-  
-    // Empty the region associated with the articles
-    clear();
-  
-    // Build the query URL for the ajax request to the NYT API
-    var queryURL = buildQueryURL();
-  
-    // Make the AJAX request to the API - GETs the JSON data at the queryURL.
-    // The data then gets passed as an argument to the updatePage function
     $.ajax({
-      url: queryURL,
-      method: "GET"
-    }).then(updatePage);
-  });
+        url: queryURL,
+        method: "GET"
+    })
+    .then(function(response) {
+
+        // Log the queryURL
+        console.log(queryURL);
+
+        // Log the resulting object
+        console.log(response);
+    });
+}
+
+function renderData(){
+
+}
